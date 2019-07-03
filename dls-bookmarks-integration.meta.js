16b50cecb0af17f82fe08f9f64bdedadd60a714c
Marco Manieri (ufficio) <marco@italiatekno.it>
2019-07-03 15:37:17 +0200

    one @match to rule them all

--- dls-bookmarks-integration.meta.js
+++ dls-bookmarks-integration.meta.js
@@ -3,15 +3,12 @@
 // @name           Bookmarks Integration for Destroyed Links Simulator
 // @description    Stores Destroyed Links Simulator information into Bookmarks
 // @category       Misc
-// @version        1.5
+// @version        1.6
 // @author         MarcioPG
 // @website        https://github.com/manierim/destroyed-links-simulator-bookmarks-integration
 // @updateURL      https://github.com/manierim/destroyed-links-simulator-bookmarks-integration/raw/master/dls-bookmarks-integration.meta.js
 // @downloadURL    https://github.com/manierim/destroyed-links-simulator-bookmarks-integration/raw/master/dls-bookmarks-integration.user.js
 // @namespace      https://github.com/manierim
-// @include        *://*.ingress.com/intel*
-// @include        *://intel.ingress.com/*
-// @match          *://*.ingress.com/intel*
-// @match          *://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
